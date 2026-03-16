d5bc26c66031997cee9d5035ca0154dd7252461c
Mjvwl <mohmmedjalal9@gmail.com>
2026-03-16 13:52:17 +0000

    Update  upload.php

--- upload / upload.php
+++ upload / upload.php
@@ -2,3 +2,40 @@
 echo "Upload feature page";
 ?>
 
+
+<?php
+
+if(isset($_POST['upload'])){
+
+$target = "uploads/" . basename($_FILES["file"]["name"]);
+
+move_uploaded_file($_FILES["file"]["tmp_name"], $target);
+
+echo "File uploaded successfully";
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Upload File</title>
+</head>
+
+<body>
+
+<h2>Upload File</h2>
+
+<form method="POST" enctype="multipart/form-data">
+
+<input type="file" name="file">
+
+<button type="submit" name="upload">Upload</button>
+
+</form>
+
+</body>
+</html>
+
+
